0259502c4be8c500836f101ab6552f57866b4504
Harman Randhawa <Harmanjit Randhawa>
2024-08-25 15:00:53 -0600

    created dynamic data source config

---
+++ DatabaseProxyService/src/main/java/com/DatabaseProxyService/config/DynamicDataSourceConfig.java
@@ -0,0 +1,39 @@
+package com.databaseproxyservice.config;
+
+import com.example.properties.DatabaseProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class DynamicDataSourceConfig {
+
+    private final DatabaseProperties databaseProperties;
+
+    public DynamicDataSourceConfig(DatabaseProperties databaseProperties) {
+        this.databaseProperties = databaseProperties;
+    }
+
+    @Bean
+    @Primary
+    public Map<String, DataSource> dataSources() {
+        Map<String, DataSource> dataSources = new HashMap<>();
+
+        databaseProperties.getDb().forEach((key, props) -> {
+            DriverManagerDataSource dataSource = new DriverManagerDataSource();
+            dataSource.setUrl(props.getUrl());
+            dataSource.setUsername(props.getUsername());
+            dataSource.setPassword(props.getPassword());
+            dataSource.setDriverClassName(props.getDriverClassName());
+
+            dataSources.put(key, dataSource);
+        });
+
+        return dataSources;
+    }
+}
